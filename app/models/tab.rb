d692d0bd6fe4f6d7ab241efe551458150aab2a79
andya <andya@wealthfront.com>
2016-08-21 13:43:41 -0700

    update specs to pass

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -3,7 +3,7 @@ class Tab < ActiveRecord::Base
   has_many :items
   has_and_belongs_to_many :rabbits
 
-  before_create do 
-    dine_date = Date.today
+  before_create do
+    self.dine_date = Date.today
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Item, type: :model do
   let(:item) { Item.new }
 
   it 'should have methods #tab and #rabbits' do
-    [:tab, :rabbits].each { |method| item.should respond_to method }
+    [:tab, :rabbits].each { |method| expect(item).to respond_to(method) }
   end
 end
